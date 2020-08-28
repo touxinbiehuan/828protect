858af4f9e7e3573c5467fe44f6275161d05e0542
touxinbiehuan <2541180485@qq.com>
2020-08-28 21:16:32 +0800

    完成了退出登录和渲染用户信息

--- js/axios.defaults.js
+++ js/axios.defaults.js
@@ -1,5 +1,12 @@
 // 对axios进行二次封装
-axios.defaults.baseURL="http://localhost:8888";
+// axios.defaults.baseURL="http://localhost:8888";
+
+// loaclhost和127.0.0.1:8888之间也是有跨域的
+axios.defaults.baseURL="http://127.0.0.1:8888";
+
+// 配置为true 后台的请求都会带上cookie
+axios.defaults.withCredentials=true;
+
 // 数据以表单的形式扔给服务器
 axios.defaults.headers['Content-Type']='application/x-www-form-urlencoded';
 
